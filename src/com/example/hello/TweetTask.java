3690d8a519737c54000f9afaf2c1512df19f31a1
tddcomputer3 <training+tddcomputer3@thoughtbot.com>
2011-02-11 16:57:20 -0500

    Post to Twitter

--- src/com/example/hello/TweetTask.java
+++ src/com/example/hello/TweetTask.java
@@ -1,10 +1,34 @@
 package com.example.hello;
 
+import android.os.AsyncTask;
+import twitter4j.http.AccessToken;
+import twitter4j.TwitterFactory;
+import twitter4j.TwitterException;
+import twitter4j.Twitter;
+
 import android.util.Log;
 
-class TweetTask {
-  TweetTask(Object a) {}
-  public void execute() {
-    Log.i("TweetTask", "Ready to post to Twitter!");
+class TweetTask extends AsyncTask<Void, Void, Integer> {
+  private AccessToken token;
+
+  TweetTask(AccessToken a) {
+    this.token = a;
+  }
+
+  protected Integer doInBackground(Void... ignored) {
+    try {
+      twitter().updateStatus("Hello, Twitter!");
+    } catch (TwitterException e) {
+      Log.i("TweetTask", e.toString());
+    }
+
+    return 0;
+  }
+
+  private Twitter twitter() {
+    return new TwitterFactory().getOAuthAuthorizedInstance(
+        "Rhq0rKl7sGHSkpAxDGOhSg",
+        "DlUPKHFFzX3jFMIqkzm7eYe4GkkLfS716rWCeLj6Uk",
+        this.token);
   }
 }
